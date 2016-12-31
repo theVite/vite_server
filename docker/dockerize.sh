86a4175db69aaffbbb1d367b1800c65f8aa61b20
Sage.Smith <sage.smith@workiva.com>
2016-12-31 13:41:17 -0700

    Remove quotes

--- docker/dockerize.sh
+++ docker/dockerize.sh
@@ -6,6 +6,6 @@ USER=thevite
 DOCKER_NAME="${USER}/${NAME}"
 TAG=${1:-latest}
 
-mv "build/libs/${NAME}-*.jar" "docker/${NAME}.jar"
-docker build docker/ -t "${DOCKER_NAME}:${TAG}"
-rm "docker/${NAME}.jar"
+cp build/libs/${NAME}-*.jar docker/${NAME}.jar
+docker build docker/ -t ${DOCKER_NAME}:${TAG}
+rm docker/${NAME}.jar
