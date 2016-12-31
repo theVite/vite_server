62541437e83c397398baea58f3e513550596ca48
Sage.Smith <sage.smith@workiva.com>
2016-12-31 02:08:22 -0700

    Change dockerize script

--- docker/dockerize.sh
+++ docker/dockerize.sh
@@ -3,7 +3,7 @@ set -ex
 NAME=vite-server
 TAG=${1:-test}
 
-./gradlew build
+#./gradlew build
 mv build/libs/$NAME-*.jar docker/$NAME.jar
 docker build docker/ -t "${NAME}:${TAG}"
 rm docker/$NAME.jar
