0d992cd12e77dcf19ba8067494dac69c1c1fc4ba
Sage.Smith <sage.smith@workiva.com>
2016-12-31 04:06:29 -0700

    cleanup

--- docker/dockerize.sh
+++ docker/dockerize.sh
@@ -3,7 +3,6 @@ set -ex
 NAME=thevite/vite_server
 TAG=${1:-latest}
 
-#./gradlew build
 mv build/libs/vite-server-*.jar docker/vite-server.jar
 docker build docker/ -t "${NAME}:${TAG}"
 rm docker/vite-server.jar
