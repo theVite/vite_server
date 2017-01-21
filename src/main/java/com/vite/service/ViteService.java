6655c841b8a61aa18b918ba1ae22c40ac67531de
Sage.Smith <sage.smith@workiva.com>
2017-01-20 23:06:59 -0700

    Created the service

--- src/main/java/com/vite/service/ViteService.java
+++ src/main/java/com/vite/service/ViteService.java
@@ -4,14 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+
 /**
  * Class that holds the implementation of the Service interface
  */
 @Component
-public class ViteService { //implements Service.Iface {
+public class ViteService implements Service.Iface {
     private static final Logger LOGGER = LoggerFactory.getLogger(ViteService.class);
     
-    //@Override
+    @Override
     public void ping() {
         LOGGER.debug("Server Pinged");
     }
