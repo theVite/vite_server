c2a27f077f1a29921c282395eab1cdeed5253306
Sage.Smith <sage.smith@workiva.com>
2017-01-22 21:34:17 -0700

    Added todo to the unit test

--- src/test/java/com/vite/service/ViteHandlerTest.java
+++ src/test/java/com/vite/service/ViteHandlerTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 public class ViteHandlerTest {
     /**
      * Tests the {@link ViteHandler#ping()} function
+     * TODO add assertion about log statement, or better yet make ping return a String
      */
     @Test
     public void testPing() {
